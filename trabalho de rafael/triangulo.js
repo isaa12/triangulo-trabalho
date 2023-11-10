dead04208ad8efa7a51bcafe3019d97ddd37d617
isaa12 <128372592+isaa12@users.noreply.github.com>
2023-11-10 12:09:06 -0300

    Add files via upload

---
+++ trabalho de rafael/triangulo.js
@@ -0,0 +1,9 @@
+function calcular(){
+    let base = parseFloat(prompt("Digite o valor da base:"));
+    let altura = parseFloat(prompt("Digite o valor da altura:"));
+    let area = base * altura/2;
+
+    alert("o valor da area do triangulo é" + area);
+}
+
+calcular()
